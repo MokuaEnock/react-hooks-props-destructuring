05209dec492fadb23e7e0b84b508c6e55e722810
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-09-20 17:35:00 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
+//Parent Component
 function App() {
-  const title = "Mad Max";
-  const posterURL =
+  let title = "Mad Max";
+  let posterUrl =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
-  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+  let genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc={posterUrl} genres={genresArr} />
     </div>
   );
 }
